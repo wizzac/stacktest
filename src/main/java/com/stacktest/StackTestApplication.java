0013dbac561a88eb123df32a3bab85903f828cc9
David <dpacheco@stacktrace.com.ar>
2019-09-10 11:02:01 -0300

    Reviento todo! nuevo proyecto spring

---
+++ src/main/java/com/stacktest/StackTestApplication.java
@@ -0,0 +1,13 @@
+package com.stacktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacktest/hcd_api/LoginTest.java
@@ -0,0 +1,10 @@
+package com.stacktest.hcd_api;
+
+import org.junit.Test;
+
+public class LoginTest {
+	@Test
+	public void doLogin() {
+		assert true;
+	}
+}
