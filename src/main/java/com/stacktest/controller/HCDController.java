ac56ee32d8dda77732ca9dab15092202fcb74c1e
David <dpacheco@stacktrace.com.ar>
2019-09-12 14:31:24 -0300

    Nuevo Display para ServicesDisplay

--- src/main/java/com/stacktest/controller/HCDController.java
+++ src/main/java/com/stacktest/controller/HCDController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HCDController {	
 	@GetMapping("/hcd/display")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String display(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "hcd/display";
+    }
+	
+	@GetMapping("/hcd/displayfulldto")
+    public String displayfulldto(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hcd/displayfulldto";
     }
 }
